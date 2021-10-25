56cb4539ba70f49841c233483e172df7a8ce58eb
Documenter.jl <documenter@juliadocs.github.io>
2021-10-25 11:41:37 +0000

    build based on f14ad3f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FractionalDiffEq","category":"page"},{"location":"#FractionalDiffEq","page":"Home","title":"FractionalDiffEq","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FractionalDiffEq.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FractionalDiffEq]","category":"page"}]
+}
