4d9c4233cab9b7c8789d8fd578a2f725b78b3295
ErikQQY <2283984853@qq.com>
2021-11-08 12:53:38 +0800

    Fix get start docs
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- docs/make.jl
+++ docs/make.jl
@@ -15,6 +15,7 @@ makedocs(;
     ),
     pages=[
         "FractionalDiffEq.jl" => "index.md",
+        "Get Start" => "utils.md",
         "Examples" => "example/example.md",
         "FractionalDiffEq APIs" => "APIs.md"
     ],
