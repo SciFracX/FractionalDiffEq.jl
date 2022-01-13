68091989c26e1b4a4962b7684df2a91ff569cc62
ErikQQY <2283984853@qq.com>
2022-01-13 23:09:52 +0800

    Update single term example
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- examples/example.jl
+++ examples/example.jl
@@ -1,20 +1,19 @@
 using FractionalDiffEq
 using Plots, LaTeXStrings
 
-#Analytical solution
-target = []
+# Analytical solution
+analytical(x) = x.^1.8 .*mittleff(1.8, 2.8, -x.^1.8)
 
-for i in 0:0.01:20
-    push!(target, i^1.8*mittleff(1.8,2.8,-i^1.8))
-end
+
 
 s="\$D^{1.8}y(x)=1-y(x),\\ y(0)=0\$"
 
-#Numerical solution
+# Numerical solution
 fun(x, y) = 1-y
 prob = SingleTermFODEProblem(fun, 1.8, 0.01)
 result=solve(prob, 0, 20, PECE())
 tspan=collect(0:0.01:20)
+target = analytical(tspan)
 
 gr()
 
