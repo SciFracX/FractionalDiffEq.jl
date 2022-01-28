d1cba67253876b10593068afd353c0d60b1cf70e
ErikQQY <2283984853@qq.com>
2022-01-28 23:31:49 +0800

    Cover more tests
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- test/auxillary.jl
+++ test/auxillary.jl
@@ -16,9 +16,19 @@ end
 @testset "Test Mittag Leffler function" begin
     @test isapprox(mittleff(1, 2, 1), exp(1)-1; atol=1e-5)
     @test isapprox(mittleff(2, 2, 1), sinh(1); atol=1e-5)
+    @test isapprox(mittleff(1, 1, 0), 1; atol=1e-5)
     @test isapprox(mittlefferr(1, 1, 1, 1), 2.718281828459045; atol=1e-5)
     @test isapprox(mittlefferr(1, 1, 1), 2.718281828459045; atol=1e-5)
 
     @test isapprox(mittleffderiv(1, 1, 1), 2.718281828459045; atol=1e-5)
     @test isapprox(mittleffderiv(1, 1), 2.718281828459045; atol=1e-5)
+
+    @test isapprox(mittleffderiv(1, 1, 1/3), 1.39561242508609; atol=1e-5)
+    @test isapprox(mittleffderiv(2, 3, 1), 0.18393972058572117; atol=1e-5)
+end
+
+@testset "Test meshgrid" begin
+    a, b = meshgrid(0:1, 0:1)
+    isapprox(a, [0 1; 0 1]; atol=1e-2)
+    isapprox(b, [0 0; 1 1]; atol=1e-2)
 end
