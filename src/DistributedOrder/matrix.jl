25ecf50ae19bc702b9d0aad0e72165610b142cac
ErikQQY <2283984853@qq.com>
2022-02-03 23:43:49 +0800

    Add references
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/DistributedOrder/matrix.jl
+++ src/DistributedOrder/matrix.jl
@@ -2,6 +2,19 @@ import FractionalDiffEq: FractionalDiffEqAlgorithm, solve, eliminator, DOB
 
 using LinearAlgebra
 
+"""
+
+### References
+
+```tex
+@inproceedings{Jiao2012DistributedOrderDS,
+  title={Distributed-Order Dynamic Systems - Stability, Simulation, Applications and Perspectives},
+  author={Zhuang Jiao and Yang Quan Chen and Igor Podlubny},
+  booktitle={Springer Briefs in Electrical and Computer Engineering},
+  year={2012}
+}
+```
+"""
 struct DOMatrixDiscrete <: FractionalDiffEqAlgorithm end
 
 function solve(ω, t, h, B, ::DOMatrixDiscrete)
