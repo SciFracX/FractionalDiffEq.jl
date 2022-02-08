95b99b168787a8745b5f20c61d331bae63145dc7
ErikQQY <2283984853@qq.com>
2022-02-08 19:24:13 +0800

    Update example and docs for DO
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- examples/dorelaxation.jl
+++ examples/dorelaxation.jl
@@ -2,9 +2,8 @@ using FractionalDiffEq, Plots, LaTeXStrings
 
 s = "\$Distributed\\ Order\\ Relaxation\$"
 
-h = 0.01
-t = collect(0:h:5)
-result = solve(x->6*x*(1-x), t, h, 0.1, DOMatrixDiscrete())
-
-using Plots
+h = 0.01; t = collect(h:h:5);
+fun(t)=-0.1
+prob = DODEProblem([1, 0.1], [x->6*x*(1-x), 0], [0, 1], t, h, fun)
+result = solve(prob, DOMatrixDiscrete())
 plot(t, result, title=s)
--- src/DistributedOrder/matrix.jl
+++ src/DistributedOrder/matrix.jl
@@ -68,7 +68,7 @@ end
 
 #=
 h = 0.01; t = collect(h:h:5);
-fun(t)=1
+fun(t)=-0.1
 prob = DODEProblem([1, 0.1], [x->6*x*(1-x), 0], [0, 1], t, h, fun)
 
 result = solve(prob, DOMatrixDiscrete())
