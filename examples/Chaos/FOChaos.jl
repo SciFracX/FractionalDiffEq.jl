2fcd206bbfa7ad9d4bd4a602ae8304210e34437c
ErikQQY <2283984853@qq.com>
2022-03-10 13:22:56 +0800

    Add more chaotic system examples
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- examples/Chaos/FOChaos.jl
+++ examples/Chaos/FOChaos.jl
@@ -112,3 +112,99 @@ result = solve(prob, h, tf, GLWithMemory())
 
 using Plots
 plot3d(result[:, 1], result[:, 2], result[:, 3], title="Fractional Order Lotka Volterra System")
+
+
+#=======Fractional Order Lu System=======#
+using FractionalDiffEq
+
+h=0.005
+alpha = [0.985, 0.99, 0.98]
+x0 = [0.2, 0.5, 0.3]
+tf=60
+function f(t, x, y, z, k)
+    a, b, c = 36, 3, 20
+    if k == 1
+        return a*(y-x)
+    elseif k == 2
+        return -x*z+c*y
+    elseif k == 3
+        return x*y-b*z
+    end
+end
+prob = FODESystem(f, alpha, x0)
+result = solve(prob, h, tf, GLWithMemory())
+
+using Plots
+plot3d(result[:, 1], result[:, 2], result[:, 3], title="Fractional Order Lu System")
+
+
+#=======Fractional Order Rossler System=======#
+using FractionalDiffEq
+
+h=0.005
+alpha = [0.9, 0.85, 0.95]
+x0 = [0.5, 1.5, 0.1]
+tf=120
+function f(t, x, y, z, k)
+    a, b, c = 0.5, 0.2, 10
+    if k == 1
+        return -y-z
+    elseif k == 2
+        return x+a*y
+    elseif k == 3
+        return b+z*(x-c)
+    end
+end
+prob = FODESystem(f, alpha, x0)
+result = solve(prob, h, tf, GLWithMemory())
+
+using Plots
+plot3d(result[:, 1], result[:, 2], result[:, 3], title="Fractional Order Rossler System")
+
+
+#=======Fractional Order Arneodo System=======#
+using FractionalDiffEq
+
+h=0.005
+alpha = [0.97, 0.97, 0.96]
+x0 = [-0.2, 0.5, 0.2]
+tf=100
+function f(t, x, y, z, k)
+    b1, b2, b3, b4 = -5.5, 3.5, 0.8, -1.0
+    if k == 1
+        return y
+    elseif k == 2
+        return z
+    elseif k == 3
+        return -b1*x-b2*y-b3*z+b4*x^3
+    end
+end
+prob = FODESystem(f, alpha, x0)
+result = solve(prob, h, tf, GLWithMemory())
+
+using Plots
+plot3d(result[:, 1], result[:, 2], result[:, 3], title="Fractional Order Arneodo System")
+
+
+#=======Fractional Order Genesio-Tesi System=======#
+using FractionalDiffEq
+
+h=0.005
+alpha = [0.95, 0.95, 0.95]
+x0 = [-0.1, 0.5, 0.2]
+tf=100
+function f(t, x, y, z, k)
+    b1, b2, b3, b4 = 1, 1.1, 0.4, 1.0
+    if k == 1
+        return y
+    elseif k == 2
+        return z
+    elseif k == 3
+        return -b1*x-b2*y-b3*z+b4*x^2
+    end
+end
+prob = FODESystem(f, alpha, x0)
+result = solve(prob, h, tf, GLWithMemory())
+
+using Plots
+plot3d(result[:, 1], result[:, 2], result[:, 3], title="Fractional Order Genesio-Tesi System")
