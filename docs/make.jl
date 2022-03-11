402644c8b13b4b67b2e4b3d08b252b767cd74d64
ErikQQY <2283984853@qq.com>
2022-03-11 11:58:51 +0800

    Refactor file deps
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using FractionalDiffEq, Documenter, DemoCards, JSON
 
-ChaosGallery, postprocess_cb, ChaosGallery_assets = makedemos("src/ChaosGallery")
+ChaosGallery, postprocess_cb, ChaosGallery_assets = makedemos("ChaosGallery")
 
 makedocs(;
     modules=[FractionalDiffEq],
