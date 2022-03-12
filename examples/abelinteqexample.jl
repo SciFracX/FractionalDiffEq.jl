6245f8b37f516bb9351a273ba27fa2e72804829f
ErikQQY <2283984853@qq.com>
2022-03-12 09:48:49 +0800

    Add conversion matrix for US polynomials
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- examples/abelinteqexample.jl
+++ examples/abelinteqexample.jl
@@ -1,7 +1,9 @@
-using FractionalDiffEq, Plots
-
+using FractionalDiffEq, Plots, SpecialFunctions
+analytical(x)=exp(1+x)*erfc(sqrt(1+x))
 e(x)=1+0*x
 f(x)=0*x
 xx = LinRange(-1, 1, 100)
 sol = solve(f, e, 20, xx, SpectralUltraspherical())
-plot(xx, sol)
+solanalytical = analytical.(xx)
+plot(xx, sol, title="Second kind Abel integral equation", label="Numerical")
+plot!(xx, solanalytical, ls=:dash, label="Analytical")
