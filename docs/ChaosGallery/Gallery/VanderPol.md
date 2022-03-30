cb7f15154036b4b8ec470d73c0641386c0b87070
ErikQQY <2283984853@qq.com>
2022-03-30 10:19:36 +0800

    Update example
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- docs/ChaosGallery/Gallery/VanderPol.md
+++ docs/ChaosGallery/Gallery/VanderPol.md
@@ -9,7 +9,7 @@ x0 = [0.2, -0.2]
 tf=60
 function VanderPol!(du, u, p, t)
     ϵ = 1
-    du[1] = y
+    du[1] = u[2]
     du[2] = -u[1]-ϵ*(u[1]^2-1)*u[2]
 end
 prob = FODESystem(VanderPol!, alpha, x0)
---
+++ examples/classical.jl
@@ -0,0 +1,14 @@
+using FractionalDiffEq, Plots
+
+function classical!(du, u, p, t)
+    A, B = 1, 3
+    du[1]=A-(B+1)*u[1]+u[1]^2*u[2]
+    du[2]=B*u[1]-u[1]^2*u[2]
+end
+α = [0.8, 0.7]
+u0 = [1.2, 2.8]
+h=0.01
+prob = FODESystem(classical!, α, u0)
+result = solve(prob, h, 10, NonLinearAlg())
+tspan = collect(0:h:10)
+plot(result[:, 1], result[:, 2])
