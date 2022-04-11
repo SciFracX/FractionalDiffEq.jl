75d34e0b27c4c72ca24590ecb578d64d21c05851
ErikQQY <2283984853@qq.com>
2022-04-11 23:24:53 +0800

    Enhance solution printing
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/utils.jl
+++ src/utils.jl
@@ -1,1 +1,12 @@
+function Base.show(io::IO, sol::FODESolution)
+    show(io::IO, typeof(sol.t))
+    show(stdout, MIME("text/plain"), sol.t)
+    println()
+    show(io::IO, typeof(sol.u))
+    show(stdout, MIME("text/plain"), sol.u)
+end
+
+"""
+Fractional ordinary differential equations solutions visulization.
+"""
 @recipe f(sol::FODESolution) = sol.t, sol.u
