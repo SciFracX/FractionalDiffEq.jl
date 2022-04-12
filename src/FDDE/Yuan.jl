091a1b179cb472c5e75211f0283d9b3174f02ecb
ErikQQY <2283984853@qq.com>
2022-04-12 22:48:56 +0800

    Add output after definition of problems
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/FDDE/Yuan.jl
+++ src/FDDE/Yuan.jl
@@ -24,17 +24,17 @@ function solve(FDDESys::FDDESystem, h, ::DelayABMYuan)
 	Ndelay = round(Int, τ/h)
 	m = length(ϕ)
 
-	x1=zeros(Ndelay+N+1, m)
+	x1 = zeros(Ndelay+N+1, m)
 	x1[Ndelay+N+1, :] .= 0 #Initialization
 
-	x=zeros(Ndelay+N+1, m)
+	x = zeros(Ndelay+N+1, m)
 	x[Ndelay+N+1, :] .= 0
 
 	for i=1:m
 		x[1:Ndelay, i] = ϕ[i]*ones(Ndelay)
 	end
 
-	x0=copy(x[Ndelay, :])
+	x0 = copy(x[Ndelay, :])
 
 	for i=1:m
 		αi = α[i]
@@ -46,8 +46,8 @@ function solve(FDDESys::FDDESystem, h, ::DelayABMYuan)
 		x[Ndelay+1, i]=x0[i]+h^αi*(f(x1[Ndelay+1, :]..., x[2, :]..., i)+αi*f(x0..., x[1, :]..., i))/gamma(αi+2)
 	end
 
-	M1=zeros(m)
-	N1=zeros(m)
+	M1 = zeros(m)
+	N1 = zeros(m)
 	for n=1:N
 		for i=1:m
 			αi = α[i]
@@ -55,21 +55,21 @@ function solve(FDDESys::FDDESystem, h, ::DelayABMYuan)
 			N1[i]=((n+1)^αi-n^αi)*(f(x0..., x[1, :]..., i))
 		end
 
-    for j=1:n
+		for j=1:n
+			for i=1:m
+				αi = α[i]
+				M1[i]=M1[i]+((n-j+2)^(αi+1)+(n-j)^(αi+1)-2*(n-j+1)^(αi+1))*(f(x[Ndelay+j, :]..., x[j, :]..., i))
+				N1[i]=N1[i]+((n-j+1)^αi-(n-j)^αi)*(f(x[Ndelay+j, :]..., x[j, :]..., i))
+			end
+		end
+
+    	@. x1[Ndelay+n+1, :]=x0+h^α*N1/(gamma(α)*α)
+  
 		for i=1:m
 			αi = α[i]
-			M1[i]=M1[i]+((n-j+2)^(αi+1)+(n-j)^(αi+1)-2*(n-j+1)^(αi+1))*(f(x[Ndelay+j, :]..., x[j, :]..., i))
-			N1[i]=N1[i]+((n-j+1)^αi-(n-j)^αi)*(f(x[Ndelay+j, :]..., x[j, :]..., i))
+			x[Ndelay+n+1, i]=x0[i]+h^αi*(f(x[Ndelay+n+1, :]..., x[n+1, :]..., i)+M1[i])/gamma(αi+2)
 		end
-    end
-
-    @. x1[Ndelay+n+1, :]=x0+h^α*N1/(gamma(α)*α)
-  
-    for i=1:m
-      αi = α[i]
-      x[Ndelay+n+1, i]=x0[i]+h^αi*(f(x[Ndelay+n+1, :]..., x[n+1, :]..., i)+M1[i])/gamma(αi+2)
-    end
-  end
-  return x
+	end
+	return x
 end
 
--- src/utils.jl
+++ src/utils.jl
@@ -6,7 +6,27 @@ function Base.show(io::IO, sol::FODESolution)
     show(stdout, MIME("text/plain"), sol.u)
 end
 
+function Base.show(io::IO, prob::SingleTermFODEProblem)
+    printstyled("SingleTermFODEProblem", color=:light_blue)
+    printstyled(" with order ")
+    printstyled("$(prob.α)", color=:red)
+    println()
+    println("timespan: $(prob.T)")
+    println("u0: $(prob.u0)")
+end
+
+function Base.show(io::IO, prob::MultiTermsFODEProblem)
+    printstyled("MultiTermsFODEProblem", color=:light_blue)
+    printstyled(" with order ")
+    printstyled("$(prob.orders)", color=:red)
+    println()
+    println("timespan: $(prob.T)")
+    println("u0: $(prob.u0)")
+end
+
+
+
 """
-Fractional ordinary differential equations solutions visulization.
+Fractional ordinary differential equations solutions visulization hooks.
 """
 @recipe f(sol::FODESolution) = sol.t, sol.u
