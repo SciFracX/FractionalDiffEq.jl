f4e2c2c6bfff6bf1961a56307a2d86ded35680ef
ErikQQY <2283984853@qq.com>
2022-04-21 15:02:32 +0800

    Add DOOscillator test
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- examples/dobagleytorvik.jl
+++ examples/dobagleytorvik.jl
@@ -1,5 +1,5 @@
 using FractionalDiffEq, Plots
-h=0.075; t=collect(0:h:30)
+h=0.5; t=collect(0:h:1)
 f(t) = 0 ≤ t ≤ 1 ? (return 8) : (return 0)
 prob=DODEProblem([1, 1, 1], [2, x->6*x*(1-x), 0], (0, 1), f, [0; 0], t)
 sol=solve(prob, h, DOMatrixDiscrete())
