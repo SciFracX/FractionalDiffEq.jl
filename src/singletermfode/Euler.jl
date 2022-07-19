083831d93792d6e04f7beddf4f25e6f6fc2dc7eb
Qingyu Qu <52615090+ErikQQY@users.noreply.github.com>
2022-07-19 15:20:22 +0800

    Update src/singletermfode/Euler.jl

--- src/singletermfode/Euler.jl
+++ src/singletermfode/Euler.jl
@@ -11,7 +11,7 @@ The basic forward Euler method for fractional ordinary differential equations.
 }
 ```
 """
-struct Euler <: SingleTermFDEAlgorithm end
+struct Euler <: SingleTermFODEAlgorithm end
 
 function solve(prob::SingleTermFODEProblem, h, ::Euler)
     @unpack f, α, u0, tspan = prob
