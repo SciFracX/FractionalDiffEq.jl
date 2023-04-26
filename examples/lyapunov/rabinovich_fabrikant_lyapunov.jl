f21d3e209c7e0e94627df02ce66482f268bf55f1
Qingyu Qu <52615090+ErikQQY@users.noreply.github.com>
2023-04-26 10:01:33 +0800

    Add noncommensurate lyapunov exponents (#66)
    * Add noncommensurate lyapunov exponents
    
    Signed-off-by: ErikQQY <2283984853@qq.com>
    
    * Fix comparing function
    
    Signed-off-by: ErikQQY <2283984853@qq.com>
    
    * Delete test file
    
    Signed-off-by: ErikQQY <2283984853@qq.com>
    
    * Fix CI failing
    
    Signed-off-by: ErikQQY <2283984853@qq.com>
    
    ---------
    
    Signed-off-by: ErikQQY <2283984853@qq.com>

---
+++ examples/lyapunov/rabinovich_fabrikant_lyapunov.jl
@@ -0,0 +1,30 @@
+# Commensurate case
+
+using FractionalDiffEq, Plots
+
+function RF(du, u, t)
+    du[1] = u[2]*(u[3]-1+u[1]*u[1])+0.1*u[1]
+    du[2] = u[1]*(3*u[3]+1-u[1]*u[1])+0.1*u[2]
+    du[3] = -2*u[3]*(0.98+u[1]*u[2])
+end
+
+(LE, tspan)=FOLyapunov(RF, 0.999, 0, 0.02, 300, [0.1; 0.1; 0.1], 0.005, 1000)
+plot(tspan, LE[1, :])
+plot!(tspan, LE[2, :])
+plot!(tspan, LE[3, :])
+
+
+
+# Noncommensurate case
+
+using FractionalDiffEq, Plots
+
+function LE_RF_TEST(du, u, p, t)
+    du[1] = u[2]*(u[3]-1+u[1]^2) + 0.1*u[1]
+    du[2] = u[1]*(3*u[3]+1-u[1]^2) + 0.1*u[2]
+    du[3] = -2*u[3]*(0.98+u[1]*u[2])
+end
+(LE, tspan) = FOLyapunov(LE_RF_TEST, [0.995, 0.992, 0.996], 0, 0.1, 1000, [1,1,1], 0.01, 1000)
+plot(tspan, LE[1, :])
+plot!(tspan, LE[2, :])
+plot!(tspan, LE[3, :])
--- test/FOLEtest.jl
+++ test/FOLEtest.jl
@@ -1,17 +1,30 @@
 using FractionalDiffEq
 using Test
 
-@testset "Test Lyapunov exponents" begin
+@testset "Test Commensurate Lyapunov exponents" begin
     function RF(du, u, t)
         du[1] = u[2]*(u[3]-1+u[1]*u[1])+0.1*u[1]
         du[2] = u[1]*(3*u[3]+1-u[1]*u[1])+0.1*u[2]
         du[3] = -2*u[3]*(0.98+u[1]*u[2])
     end
     
-    (LE, tspan)=FOLyapunov(RF, 0.999, 0, 0.02, 300, [0.1; 0.1; 0.1], 0.005, 1000)
+    (LE, tspan)=FOLyapunov(RF, [0.999, 0.999, 0.999], 0, 0.02, 300, [0.1; 0.1; 0.1], 0.005, 1000)
 
     # Test the latest computed(final) Lyapunov exponents
     @test isapprox(LE[end-2:end], [0.06111650166568285
     0.0038981396237095034
    -1.8324646820425692]; atol=1e-3)
+end
+
+@testset "Test Noncommensurate Lyapunov exponents" begin
+    function LE_RF_TEST(du, u, p, t)
+        du[1] = u[2]*(u[3]-1+u[1]^2) + 0.1*u[1]
+        du[2] = u[1]*(3*u[3]+1-u[1]^2) + 0.1*u[2]
+        du[3] = -2*u[3]*(0.98+u[1]*u[2])
+    end
+    (LE, tspan) = FOLyapunov(LE_RF_TEST, [0.995, 0.992, 0.996], 0, 0.1, 1000, [1,1,1], 0.01, 1000)
+    
+    @test isapprox(LE[end-2:end], [-0.0033493403496077058
+    -0.0050780674255360226
+    -1.774132087623718]; atol=1e-3)
 end
