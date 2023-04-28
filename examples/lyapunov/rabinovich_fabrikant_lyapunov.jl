dfe2aa988470de7fee784c2a07bedc83df001f4a
ErikQQY <2283984853@qq.com>
2023-04-28 15:29:34 +0800

    Add parameters specification for commensurate case
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- examples/lyapunov/rabinovich_fabrikant_lyapunov.jl
+++ examples/lyapunov/rabinovich_fabrikant_lyapunov.jl
@@ -2,14 +2,16 @@
 
 using FractionalDiffEq, Plots
 
-function RF(du, u, t)
-    du[1] = u[2]*(u[3]-1+u[1]*u[1])+0.1*u[1]
-    du[2] = u[1]*(3*u[3]+1-u[1]*u[1])+0.1*u[2]
-    du[3] = -2*u[3]*(0.98+u[1]*u[2])
+function RF(du, u, p, t)
+    a, b, c = p
+    du[1] = u[2]*(u[3]-a+u[1]*u[1])+b*u[1]
+    du[2] = u[1]*(3*u[3]+1-u[1]*u[1])+b*u[2]
+    du[3] = -2*u[3]*(c+u[1]*u[2])
 end
 
+p = [1, 0.1, 0.98]
 
-LE=FOLyapunov(RF, [0.999, 0.999, 0.999], 0, 0.02, 300, [0.1; 0.1; 0.1], 0.005, 1000)
+LE=FOLyapunov(RF, [0.999, 0.999, 0.999], 0, 0.02, 300, [0.1; 0.1; 0.1], 0.005, 1000, p)
 plot(LE)
 
 # Noncommensurate case
--- test/FOLEtest.jl
+++ test/FOLEtest.jl
@@ -16,6 +16,23 @@ using Test
    -1.8324646820425692]; atol=1e-3)
 end
 
+@testset "Test Commensurate Lyapunov exponents with parameters specification" begin
+    function RF(du, u, t)
+        du[1] = u[2]*(u[3]-1+u[1]*u[1])+0.1*u[1]
+        du[2] = u[1]*(3*u[3]+1-u[1]*u[1])+0.1*u[2]
+        du[3] = -2*u[3]*(0.98+u[1]*u[2])
+    end
+
+    p = [1, 0.1, 0.98]
+
+    LE = FOLyapunov(RF, [0.999, 0.999, 0.999], 0, 0.02, 300, [0.1; 0.1; 0.1], 0.005, 1000, p)
+
+    # Test the latest computed(final) Lyapunov exponents
+    @test isapprox(LE.LE[end-2:end], [0.06111650166568285
+    0.0038981396237095034
+   -1.8324646820425692]; atol=1e-3)
+end
+
 @testset "Test Noncommensurate Lyapunov exponents" begin
     function LE_RF_TEST(du, u, p, t)
         du[1] = u[2]*(u[3]-1+u[1]^2) + 0.1*u[1]
