6ca5c2a55d66e99ff0f028ef0271fe0b01d78ec6
ErikQQY <2283984853@qq.com>
2024-02-29 22:21:52 +0800

    No ClosedForm for now
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- test/fode.jl
+++ test/fode.jl
@@ -48,7 +48,7 @@ end
     0.26027660764161137
     0.2855329523586738]; atol=1e-3)
 end
-"""
+
 @testset "Test ClosedFormHankelM method" begin
     u0 = [0, 0, 0, 0]
     rightfun(x)=sin(x^2)
@@ -57,7 +57,7 @@ end
 
     @test isapprox(result.u, [0.0; 0.08402140107687359; 0.3754974742112727]; atol=1e-3)
 end
-
+"""
 @testset "Test GL method for FODEProblem" begin
     alpha = [0.99, 0.99, 0.99]
     u0 = [1.0, 0.0, 1.0]
